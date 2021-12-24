ae38d0f67c9ed14456edeb46d4b92ecada61ae49
Bruno-Carvalho <brunooakdev@gmail.com>
2021-12-24 17:04:08 -0300

    Subindo projeto movies

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header id="header">
+    <router-link to="/" exact>Movie Filmes</router-link>
+    <router-link tag="button" to="/salvos" exact>SALVOS</router-link>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 60px;
+  background-color: brown;
+}
+
+#header a{
+  text-decoration: none;
+  font-size: 30px;
+  color: #fff;
+  cursor: pointer;
+
+}
+
+button{
+  cursor: pointer;
+  border: 0;
+  background: #fff;
+  padding: 5px 15px;
+  outline: none;
+  border-radius: 3px;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="loading">
+    <h3>Carregando filmes...</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h3 {
+  font-size: 30px;
+}
+</style>
